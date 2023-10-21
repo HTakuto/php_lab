65c6d09a31df0d1e61a2afa9728c3c278a30d779
HTakuto <htakutolife@gmail.com>
2023-10-22 00:38:27 +0900

    trait

---
+++ object/trait.php
@@ -0,0 +1,37 @@
+<?php
+// トレイト
+trait ProductTrait{
+
+  public function getProduct(){
+    echo 'プロダクト';
+  }
+}
+
+trait NewsTrait{
+  public function getNews(){
+    echo 'ニュース';
+  }
+}
+
+class Product{
+  use ProductTrait;
+  use NewsTrait;
+
+  public function getInformation(){
+    echo 'クラスです。';
+  }
+
+  //クラス内の記述が優先される
+  // public function getNews(){
+  //   echo 'クラスのニュースです。';
+  // }
+}
+
+$product = new Product();
+
+$product->getInformation();
+echo '<br>';
+$product->getProduct();
+echo '<br>';
+$product->getNews();
+echo '<br>';
